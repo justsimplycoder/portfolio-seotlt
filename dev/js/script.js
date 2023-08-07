a305f94b694bbe69bb1e72bcb501e4850205df81
justsimplycoder <justsimplycoder@gmail.com>
2023-08-07 18:07:34 +0400

    Добавление: секция сервис

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,55 +1,48 @@
 import menu from './modules/menu.js'
-import tab from './modules/tab.js'
-import tippy from 'tippy.js';
 
 menu();
-tab();
 
-tippy('.tooltip-info', {
-	animation: 'shift-toward-extreme'
+function numberEl(index) {
+	if(index < 10) return '0' + (index + 1);
+	return index + 1;
+}
+
+// Слайдер
+const swiper1 = new Swiper('#swiper-review', {
+	direction: 'horizontal',
+	loop: true,
+	pagination: {
+		el: '.review__pagination',
+		clickable: true,
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + numberEl(index) + '</span>';
+		},
+	},
+	navigation: {
+		nextEl: '.review__btn-next',
+		prevEl: '.review__btn-prev',
+	},
+	// effect: 'fade'
 });
 
-(function() {
-	// Модуль .alert
-	function alertHandler(close) {
-		close.onclick = function (event) {
-			let alertBlock = event.target.closest('.alert');
-			if(!alertBlock) return;
-			alertBlock.classList.add('is-hide');
-			setTimeout(function() {
-				alertBlock.remove();
-			}, 400);
-		};
-	}
-	const alertClose = document.querySelectorAll('.alert__close');
-	if(alertClose.length != 0) {
-		alertClose.forEach(alertHandler);
-	}
+const swiper2 = new Swiper('#swiper-services', {
+	direction: 'horizontal',
+	// loop: true,
+	pagination: {
+		el: '.services__pagination',
+		clickable: true,
+		type: 'custom',
+		renderCustom: function (swiper2, current, total) {
+			return numberEl(current - 1) + ' <span>/ ' + numberEl(total - 1) + '</span>';
+		}
+	},
+	navigation: {
+		nextEl: '.services__btn-next',
+		prevEl: '.services__btn-prev',
+	},
+});
 
-	function numberEl(index) {
-		if(index < 10) return '0' + (index + 1);
-		return index + 1;
-	}
-
-	// Слайдер
-	const swiper = new Swiper('.swiper', {
-		// Optional parameters
-		direction: 'horizontal',
-		loop: true,
-
-		// If we need pagination
-		pagination: {
-			el: '.review__pagination',
-			clickable: true,
-			renderBullet: function (index, className) {
-				return '<span class="' + className + '">' + numberEl(index) + '</span>';
-			},
-		},
-
-		// Navigation arrows
-		navigation: {
-			nextEl: '.review__btn-next',
-			prevEl: '.review__btn-prev',
-		},
-	});
-})();
+swiper2.navigationNext = (swiper) => {
+	console.log('sdf');
+}
+console.log(swiper2.nextEl);
